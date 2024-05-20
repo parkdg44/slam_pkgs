342d117c23335b4fa3dbfdb8b2dd428d36e38ff5
데릭_박동규 <derrick@clobot.co.kr>
2024-05-20 22:44:43 +0900

    <feat> ros2 기능 추가

--- slam_main/include/slam_pkg/ros2/ros2_converter.hpp
+++ slam_main/include/slam_pkg/ros2/ros2_converter.hpp
@@ -5,10 +5,15 @@
 #pragma once
 
 #include <geometry_msgs/msg/pose_stamped.hpp>
+#include <geometry_msgs/msg/twist.hpp>
+#include <sensor_msgs/msg/laser_scan.hpp>
+#include <sensor_msgs/msg/point_cloud.hpp>
 
-#include "type/types.hpp"
+#include "slam_pkg/type/types.hpp"
 
 namespace Slam::ros2 {
+
+#pragma region to_ros
 
 inline builtin_interfaces::msg::Time to_ros(Time const& data) { return rclcpp::Time(data.value); }
 
@@ -36,25 +41,86 @@ inline geometry_msgs::msg::Quaternion to_ros(Quaternion const& data) {
   return output;
 }
 
-inline geometry_msgs::msg::PoseStamped to_ros(Pose const& data) {
+inline geometry_msgs::msg::PoseStamped to_ros(PoseStamped const& data) {
   geometry_msgs::msg::PoseStamped output;
   output.header = to_ros(data.header);
-  output.pose.position = to_ros(data.p);
-  output.pose.orientation = to_ros(data.q);
+  output.pose.position = to_ros(data.pose.p);
+  output.pose.orientation = to_ros(data.pose.q);
   return output;
 }
 
+inline geometry_msgs::msg::Vector3 to_ros(Vector const& data) {
+  geometry_msgs::msg::Vector3 output;
+  output.x = data.x;
+  output.y = data.y;
+  output.z = data.z;
+  return output;
+}
+
+inline geometry_msgs::msg::Twist to_ros(Twist const& data) {
+  geometry_msgs::msg::Twist output;
+  output.angular = to_ros(data.angular);
+  output.linear = to_ros(data.linear);
+  return output;
+}
+
+#pragma endregion to_ros
+
+#pragma region from_ros
+
+inline Header from_ros(std_msgs::msg::Header const& data) {
+  Header output;
+  output.ref_frame_id = data.frame_id;
+  output.stamp = rclcpp::Time(data.stamp).nanoseconds();
+  return output;
+}
+
+inline LaserScan from_ros(sensor_msgs::msg::LaserScan const& data) {
+  LaserScan output;
+  output.header = from_ros(data.header);
+  output.ranges = data.ranges;
+  output.intensities = data.intensities;
+  ScanInfo& info = output.info;
+  info.angle_increment = data.angle_increment;
+  info.angle_max = data.angle_max;
+  info.angle_min = data.angle_min;
+  info.range_max = data.range_max;
+  info.range_min = data.range_min;
+  info.scan_time = data.scan_time;
+  info.time_increment = data.time_increment;
+  return output;
+}
+
+inline Point from_ros(geometry_msgs::msg::Point32 const& data) {
+  Point output;
+  output.x = data.x;
+  output.y = data.y;
+  output.z = data.z;
+  return output;
+}
+
+inline PointCloud<Point> from_ros(sensor_msgs::msg::PointCloud const& data) {
+  PointCloud<Point> output;
+  output.header = from_ros(data.header);
+  output.points.reserve(data.points.size());
+  for (const auto& p : data.points) {
+    output.points.push_back(from_ros(p));
+  }
+  return output;
+}
+
+#pragma endregion from_ros
+
 template <typename SlamT>
-std::function<void(std::string const&, SlamT const&)> ros2_function_builder(rclcpp::Node& node) {
+std::function<void(std::string const&, SlamT const&)> pub_function_builder(rclcpp::Node& node) {
   return [&node](std::string const& topic, SlamT const& data) {
-    using RosT = decltype(to_ros(data));
+    using RosT = decltype(to_ros(std::declval<SlamT>()));
     static auto map_pub = std::unordered_map<std::string, std::shared_ptr<rclcpp::PublisherBase>>();
 
     if (map_pub.find(topic) == map_pub.end()) {
       map_pub[topic] = node.create_publisher<RosT>(topic);
     }
     auto pub_data = std::static_pointer_cast<rclcpp::Publisher<RosT>>(map_pub[topic]);
-
     pub_data->publish(to_ros(data));
   };
 }
--- slam_main/include/slam_pkg/ros2/ros2_node.hpp
+++ slam_main/include/slam_pkg/ros2/ros2_node.hpp
@@ -4,20 +4,51 @@
 
 #pragma once
 
-#include <geometry_msgs/msg/pose_stamped.hpp>
 #include <memory>
 #include <rclcpp/rclcpp.hpp>
 
-#include "ros2/ros2_converter.hpp"
+#include "slam_pkg/ros2/ros2_converter.hpp"
+#include "slam_pkg/util/string_helper/string_helper.hpp"
 
 namespace Slam::ros2 {
 
 class Node : public rclcpp::Node {
+  template <typename T>
+  using SubPtr = typename rclcpp::Subscription<T>::SharedPtr;
+
  public:
-  Node() : rclcpp::Node("slam_main") { RCLCPP_INFO(get_logger(), "aaa"); }
+  Node() : rclcpp::Node("slam_main") {
+    declare_parameter("laser_topics", "");
+    declare_parameter("pcd_topics", "");
+
+    auto laser_topics = util::split_str(get_parameter("laser_topics").as_string(), ' ');
+    auto pcd_topics = util::split_str(get_parameter("pcd_topics").as_string(), ' ');
+
+    for (const auto& topic : laser_topics) {
+      laser_map_[topic] = create_subscription<sensor_msgs::msg::LaserScan>(
+          topic, rclcpp::SensorDataQoS(),
+          [this, topic](const sensor_msgs::msg::LaserScan& data) { laser_callback(topic, data); });
+    }
+
+    for (const auto& topic : laser_topics) {
+      laser_map_[topic] = create_subscription<sensor_msgs::msg::LaserScan>(
+          topic, rclcpp::SensorDataQoS(),
+          [this, topic](const sensor_msgs::msg::LaserScan& data) { laser_callback(topic, data); });
+    }
+  }
+
+  void laser_callback(const std::string& topic, const sensor_msgs::msg::LaserScan& data) {
+    auto scan = from_ros(data);
+  }
+
+  void laser_callback(const std::string& topic, const sensor_msgs::msg::PointCloud& data) {
+    auto pcd = from_ros(data);
+  }
 
  private:
-  rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr pub_pose_;
+  std::unordered_map<std::string, SubPtr<sensor_msgs::msg::LaserScan>> laser_map_{};
+  std::unordered_map<std::string, SubPtr<sensor_msgs::msg::PointCloud>> pcd_map_{};
+  LaserScan scan_{};
 };
 
 }  // namespace Slam::ros2
